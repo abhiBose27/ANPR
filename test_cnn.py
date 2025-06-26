8cb7d7c9c5e848939f7de087524686e2e63a5c08
Abhishek <boseabhishek2@gmail.com>
2025-06-26 17:50:15 +0200

    [Test CNN] Fix the code

--- test_cnn.py
+++ test_cnn.py
@@ -1,8 +1,9 @@
 import cv2
-from prediction import get_plate_string
+from prediction import Prediction
 
 if __name__ == "__main__":
     image_path = "test_images/test_plate.png"
     plate_image = cv2.imread(image_path)
-    plate_string = get_plate_string(plate_image)
+    prediction = Prediction("cnn_model/best.keras", debug=True)
+    plate_string = prediction.get_plate_string(plate_image)
     print("🔤 Recognized Plate:", plate_string)
